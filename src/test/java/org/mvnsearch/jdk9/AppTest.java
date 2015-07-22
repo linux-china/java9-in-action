93c63a1d61750e08d14ec3ffe02d2c0fd2226d06
linux_china <linux_china@hotmail.com>
2015-07-22 09:55:16 +0800

    Unit test for simple App

---
+++ src/test/java/org/mvnsearch/jdk9/AppTest.java
@@ -0,0 +1,20 @@
+package org.mvnsearch.jdk9;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ *
+ * @author linux_china
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        Assert.assertTrue(true);
+    }
+}
