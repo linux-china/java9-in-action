dddd367ac81e9e7000918b0e2b7316466604661f
linux_china <linux_china@hotmail.com>
2015-07-22 09:54:32 +0800

    app entrance

---
+++ src/main/java/org/mvnsearch/jdk9/App.java
@@ -0,0 +1,12 @@
+package org.mvnsearch.jdk9;
+
+/**
+ * app entrance
+ *
+ * @author linux_china
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
