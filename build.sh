aade232f277595809517ada992e47f0354cd2924
linux_china <linux_china@hotmail.com>
2016-08-24 00:52:01 -0700

    add wild match compile

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 GREEN='\033[0;32m'
 NC='\033[0m'
+MODULE_PATH="target/mods"
 
 rm -rf target
 
-mkdir -p target/mods
+mkdir -p ${MODULE_PATH}
 
 echo "${GREEN}building astro${NC}"
 
@@ -11,9 +12,9 @@ rm -rf astro/target
 
 mkdir -p astro/target/classes
 
-javac -d astro/target/classes astro/src/main/java/module-info.java astro/src/main/java/org/astro/World.java
+find astro/src/main/java -type f -name "*.java" -print | xargs javac -d astro/target/classes
 
-jar -cfe target/mods/astro.jar org.astro.World -C astro/target/classes .
+jar -cfe ${MODULE_PATH}/astro.jar org.astro.Main -C astro/target/classes .
 
 echo "${GREEN}building greetings${NC}"
 
@@ -21,17 +22,17 @@ rm -rf greetings/target
 
 mkdir -p greetings/target/classes
 
-javac -modulepath target/mods -d greetings/target/classes greetings/src/main/java/module-info.java greetings/src/main/java/com/greetings/Main.java
+find greetings/src/main/java -type f -name "*.java" -print | xargs javac -modulepath ${MODULE_PATH} -d greetings/target/classes
 
-jar -cfe target/mods/greetings.jar com.greetings.Main -C greetings/target/classes .
+jar -cfe ${MODULE_PATH}/greetings.jar com.greetings.Main -C greetings/target/classes .
 
 echo "${GREEN}running greeting application${NC}"
 
-java -modulepath target/mods -m com.greetings/com.greetings.Main
+java -modulepath ${MODULE_PATH} -m com.greetings/com.greetings.Main
 
 echo "${GREEN}linking application${NC}"
 
-jlink --module-path target/mods/:$JAVA_HOME/jmods --add-modules com.greetings --output target/greetings-image
+jlink --module-path ${MODULE_PATH}:$JAVA_HOME/jmods --add-modules com.greetings --output target/greetings-image
 
 echo "${GREEN}run from image${NC}"
 
