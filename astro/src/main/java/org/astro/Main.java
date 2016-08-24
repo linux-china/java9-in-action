95095e404a7a47c2b022699f22258059ae804ad2
linux_china <linux_china@hotmail.com>
2016-08-24 00:52:31 -0700

    astro module Main class

---
+++ astro/src/main/java/org/astro/Main.java
@@ -0,0 +1,12 @@
+package org.astro;
+
+/**
+ * astro module Main class
+ *
+ * @author linux_china
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is the astro module");
+    }
+}
