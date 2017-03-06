49a49b9af87862742c9969d70dd65934f4533311
linux_china <linux_china@hotmail.com>
2017-03-06 15:17:28 +0800

    replace by maven build

--- build.sh
+++ build.sh
@@ -2,38 +2,10 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 MODULE_PATH="target/mods"
 
-rm -rf target
-
-mkdir -p ${MODULE_PATH}
-
-echo "${GREEN}building astro${NC}"
-
-rm -rf astro/target
-
-mkdir -p astro/target/classes
-
-find astro/src/main/java -type f -name "*.java" -print | xargs javac -d astro/target/classes
-
-jar -cfe ${MODULE_PATH}/astro.jar org.astro.Main -C astro/target/classes .
-
-echo "${GREEN}building greetings${NC}"
-
-rm -rf greetings/target
-
-mkdir -p greetings/target/classes
-
-find greetings/src/main/java -type f -name "*.java" -print | xargs javac --module-path ${MODULE_PATH} -d greetings/target/classes
-
-jar -cfe ${MODULE_PATH}/greetings.jar com.greetings.Main -C greetings/target/classes .
-
-echo "${GREEN}running greeting application${NC}"
-
-java --module-path ${MODULE_PATH} -m com.greetings/com.greetings.Main
-
-echo "${GREEN}linking application${NC}"
+mvn clean package
 
 jlink --strip-debug --module-path target/mods:$JAVA_HOME/jmods --add-modules com.greetings --output target/greetings-bin --launcher greetings=com.greetings/com.greetings.Main
 
-echo "${GREEN}run from image${NC}"
+echo "${GREEN}run from binary${NC}"
 
 ./target/greetings-bin/bin/greetings
